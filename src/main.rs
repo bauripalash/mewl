27edec6a8e95bd07594c852ce002ba3311336e21
Palash Bauri <palashbauri1@gmail.com>
2022-04-18 11:48:14 +0530

    Rewriting; basic nice errors working

--- src/main.rs
+++ src/main.rs
@@ -12,196 +12,130 @@ enum Expr {
     List(Vec<Expr>),
 }
 
+#[derive(Debug, Clone)]
+struct MewToken {
+    lexeme: String,
+    position: (usize, usize), //[line number, column position]
+}
+
 fn main() {
     let mut sym_table: HashMap<String, f64> = HashMap::new();
     sym_table.insert("age".to_string(), 20.0);
-    let a = "[::  mew]      [::  mewmew]";
+    let a = "[:: [+ mew mewmew]]
+    mewmew
+    palash
+    hello";
     let mut source_input = a.to_string();
+    source_input.push('\n');
+    source_input.push(' ');
 
-    source_input = source_input.replace('[', " [ ").replace(']', " ] ");
-
-    let mut tokens = source_input
-        .split_whitespace()
-        .map(|s| s.trim().to_string())
-        .collect::<Vec<String>>();
-    //println!("{:?}" , tokens);
-    while !tokens.is_empty() {
-        let tok_list = read_tokens(&mut tokens);
-        let _ = evaluate(&tok_list[0], &mut sym_table);
-    }
+    let _ = MewlParser::new(source_input).parse();
 }
 
-fn poptoken(tokens: &mut Vec<String>) -> String {
-    tokens.drain(..1).collect()
+#[allow(dead_code)]
+struct MewlParser {
+    source: String,
+    tokens: Vec<String>,
+    current_atom: Atom,
 }
 
-fn read_tokens(tokens: &mut Vec<String>) -> Vec<Expr> {
-    //let mut tokens = toks.clone();
-    if tokens.is_empty() {
-        eprintln!("Unexpected End found");
-    }
-    let cur_tok = poptoken(tokens);
-    match cur_tok.as_str() {
-        "[" => {
-            let mut outlist: Vec<Expr> = vec![];
-            while tokens[0] != "]" {
-                outlist.append(&mut read_tokens(tokens));
-            }
-            poptoken(tokens);
-
-            let x: Vec<Expr> = vec![Expr::List(outlist)];
-            x
-        }
-        "]" => {
-            eprintln!("Unexpected ]");
-            exit(1);
-            //panic!();
-        }
-        _ => {
-            let out: Vec<Expr> = vec![Expr::Atom(read_atom(cur_tok))];
-            //poptoken(tokens);
-            //println!("{:?}" , out);
-            out
+impl MewlParser {
+    fn new(source: String) -> Self {
+        Self {
+            source: source.replace('[', " [ ").replace(']', " ] "),
+            tokens: Vec::new(),
+            current_atom: Atom::Number(0.0),
         }
     }
-}
 
-fn read_atom(tok: String) -> Atom {
-    if tok.starts_with("mew") {
-        //let t: Vec<char> = tok.chars().into_iter().collect();
-        return Atom::Number(tok.chars().into_iter().count() as f64 / 3.0);
+    fn poktoken(&mut self) -> String {
+        self.tokens.drain(..1).collect()
     }
 
-    Atom::Sym(tok)
-}
-
-fn extract_atom(x: &Atom) -> Option<f64> {
-    match x {
-        Atom::Number(s) => Some(*s),
-        _ => None,
-    }
-}
-
-#[allow(unused_assignments)]
-fn binary_op(op: &str, largs: Vec<Atom>) -> Atom {
-    let mut result: Atom = Atom::Number(0.0);
-    let converted: Vec<Option<f64>> = largs.iter().map(extract_atom).collect();
-
-    let mut res: f64 = 0.0;
-    match op {
-        "+" => {
-            res = converted
-                .into_iter()
-                .flatten()
-                .into_iter()
-                .fold(0.0, |a, b| a + b);
-
-            //res = usable_values.into_iter().fold(0_usize, |a,b| a+b);
-        }
-
-        "-" => {
-            /*
-            res = converted
-                .into_iter()
-                .flatten()
-                .into_iter()
-                .fold(0.0, |a, b| a-b);
-            println!("{}" , res);*/
-            res = converted
-                .into_iter()
-                .flatten()
-                .reduce(|a, b| a - b)
-                .unwrap();
-        }
-
-        "*" => {
-            res = converted
-                .into_iter()
-                .flatten()
-                .into_iter()
-                .fold(1.0, |a, b| a * b);
-        }
-
-        "/" => {
-            res = converted
-                .into_iter()
-                .flatten()
-                .into_iter()
-                .reduce(|a, b| b / a)
-                .unwrap();
-        }
-
-        "::" => {
-            println!(
-                "{}",
-                converted
-                    .into_iter()
-                    .flatten()
-                    .map(|s| s.to_string())
-                    .collect::<Vec<String>>()
-                    .join(" ")
-            );
-        }
-
-        ":::" => {
-            println!(
-                "{}",
-                String::from_utf8_lossy(
-                    &converted
-                        .into_iter()
-                        .flatten()
-                        .map(|a| a as u8)
-                        .collect::<Vec<u8>>()
-                )
-            )
-        }
-        _ => {}
+    fn read_atom(&self) -> Atom {
+        unimplemented!()
     }
 
-    result = Atom::Number(res);
-    result
-}
+    fn extract_atom(&self) -> Option<f64> {
+        unimplemented!();
+    }
 
-fn evaluate<'a>(exp: &'a Expr, sym_table: &'a mut HashMap<String, f64>) -> Atom {
-    let _lang_ops: [&str; 5] = ["+", "-", "*", "/", "::"];
-    match exp {
-        Expr::Atom(atm) => match atm {
-            Atom::Number(_) => atm.to_owned(),
-            Atom::Sym(sym) => match sym.as_str() {
-                "+" | "-" | "*" | "/" | "::" | ":::" => atm.to_owned(),
-                _ => {
-                    let output = Atom::Number(*sym_table.get(sym).unwrap());
-                    output
-                }
-            },
-        },
-        Expr::List(lst) => {
-            let _l: Vec<Expr> = lst.to_vec();
-            let mut atom_list: Vec<Atom> = vec![];
-            //println!("=>{:?}" , l[0]);
-            for item in _l {
-                let x = evaluate(&item, sym_table);
-                atom_list.push(x);
+    fn get_tokens(&self) -> Vec<MewToken> {
+        let raw_toks: Vec<char> = self.source.chars().collect();
+        //println!("{:?}" , raw_toks);
+        let mut output: Vec<MewToken> = vec![];
+        let mut curp: usize = 0;
+        let mut curtok: String = String::new();
+        let mut line_no: usize = 1;
+
+        while curp < raw_toks.len() {
+            while raw_toks[curp] != ' ' {
+                if raw_toks[curp] == '\n' {
+                line_no += 1;
+                curp += 1;
+                continue;
             }
 
-            let operator: Vec<Atom> = atom_list
-                .drain(..1)
-                .into_iter()
-                //.map(|a| a)
-                .collect();
-            match &operator[0] {
-                Atom::Number(x) => {
-                    return Atom::Number(*x);
-                }
-                Atom::Sym(op) => match op.as_str() {
-                    "+" | "-" | "*" | "/" | "::" | ":::" => {
-                        return binary_op(op.as_str(), atom_list.clone());
-                    }
-                    _ => {}
-                },
+                curtok.push(raw_toks[curp]);
+                curp += 1;
             }
 
-            evaluate(&lst[0], sym_table)
+            if !curtok.is_empty() {
+                let temp_token = MewToken {
+                    lexeme: curtok.clone(),
+                    position: (line_no, curp - curtok.len()),
+                };
+                output.push(temp_token);
+                curtok = String::new();
+                //curp+=1;
+            }
+            
+            curp += 1;
         }
+
+        output
+    }
+
+    fn parse(&mut self) -> Vec<Expr> {
+        let mytoks = self.get_tokens();
+        //println!("{:#?}", mytoks);
+        //for y in mytoks{
+        //println!("{:?}" , y);
+        self.show_nice_error(&mytoks[9]);
+        //}
+        Vec::new()
+    }
+
+    fn show_nice_error(&self, tok: &MewToken) {
+        //println!("{}" , self.source);
+        let mut xx = self.source.clone();
+
+        xx.insert_str(tok.position.1 + if tok.lexeme.len() > 1 {tok.lexeme.len()-1}else{tok.lexeme.len()}, "<-\x1b[0m");
+        xx.insert_str(if tok.lexeme.len() > 1 { tok.position.1-1} else {tok.position.1}, " \x1b[93m->");
+
+        let o: Vec<String> = xx.split_terminator('\n').into_iter().map(|i| i.to_string()).collect();
+
+        let line_index = tok.position.0;
+        println!("{}-{}->{:?}" , line_index , o.len() , o);
+        if line_index > 1 {
+            println!("|{}| {}", line_index - 1, o[line_index - 2]);
+        }
+
+        println!(
+            "{}",
+            format!("|{}|", tok.position.0) + &o[tok.position.0 - 1]
+        );
+        
+        /*
+        if line_index < o.len() && line_index != o.len() {
+            println!("|{}| {}", line_index + 1, o[line_index + 1]);
+        }*/
+        
+        
+        //println!("{:?}" , tok)
+    }
+
+    fn evaluate(&mut self, sym_table: &mut HashMap<String, f64>) -> Atom {
+        Atom::Number(0.0)
     }
 }
