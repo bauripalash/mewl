afd610f1ed4df588e62b2f364c1d5c6fda46dd16
Palash Bauri <palashbauri1@gmail.com>
2022-05-05 19:45:37 +0530

    Fixed tests

--- tests/addition_test.rs
+++ tests/addition_test.rs
@@ -14,13 +14,13 @@ fn with_multiplication_test() {
 fn addition_test() {
     assert_eq!(
         13.0,
-        common::get_return_value("[+ mewmew.mew.mewmew [` mew mew]]").floor()
+        common::get_return_value("[+ mewmew.mew.mewmew [' mew mew]]").floor()
     );
     assert_eq!(
         112.0,
-        common::get_return_value("[+ mew [` mew [` mew mew]]]")
+        common::get_return_value("[+ mew [' mew [' mew mew]]]")
     );
-    assert_eq!(112.0, common::get_return_value("[+ mew [` mew mew mew]]"));
+    assert_eq!(112.0, common::get_return_value("[+ mew [' mew mew mew]]"));
     assert_eq!(3.0, common::get_return_value("[+ [+ [mew mew]] mew]"));
     assert_eq!(3.0, common::get_return_value("[+ mew mew mew]"));
     assert_eq!(2.0, common::get_return_value("[+ mew mew]"))
